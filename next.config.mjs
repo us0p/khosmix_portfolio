bb30d5a312980de56033ae2cccb6e7875cbc637c
us0p <lw98.lopes@gmail.com>
2024-05-16 13:48:08 -0300

    fix: removing puppeteer-core from webpack externals

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        return {
+            ...config,
+            externals: [...config.externals, "chrome-aws-lambda"]
+        }
+    }
+};
 
 export default nextConfig;
